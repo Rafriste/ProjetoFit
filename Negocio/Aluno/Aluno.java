fd72332e131e9ba72777b47d15c502619db03035
Rafriste <100323234+Rafriste@users.noreply.github.com>
2022-12-06 10:17:04 -0300

    Update Aluno.java

--- Negocio/Aluno/Aluno.java
+++ Negocio/Aluno/Aluno.java
@@ -10,10 +10,21 @@ public class Aluno extends InfoAluno{
 		this.senha = senha;
 		this.matr = matr;
 	}
+
+	public Aluno (int matr, String senha){
+		this.senha = senha;
+		this.matr = matr;
+	}
 	
 	public Aluno (int m){
 		this.matr = m;
 	}
+
+	public int compareTo(Aluno al) {
+        int result;
+        result = this.getSenha().compareTo(al.getSenha());
+        return result;
+    }
     
     public String toString () {
         return "MATRICULA: " + this.matr + " NOME: " + this.nome;
